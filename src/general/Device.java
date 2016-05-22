2df346f93d0f9f0318132b0969f7193f558266fa
Felix Franz <felix-franz@t-online.de>
2016-05-22 11:35:43 +0200

    Added Device and User
    Device represents clients that sends and receives messages.
    User represents a Person that can have multiple devices und uses them to
    send or receive messages.
    Each message should be sent to a user should be sent to all of his
    devices.

---
+++ src/general/Device.java
@@ -0,0 +1,15 @@
+package general;
+
+/**
+ * 
+ * @author Felix
+ * This class implements a Device
+ * Each user can have multiple Devices sending and receiving messages
+ *
+ */
+
+public class Device {
+	private User user;
+	private String lastOnline;
+	private String firstOnline;
+}
---
+++ src/general/User.java
@@ -0,0 +1,17 @@
+package general;
+
+/**
+ * 
+ * @author Felix
+ * This class implements a User
+ * Each user can have multiple Devices sending and receiving messages
+ *
+ */
+
+public class User {
+	private String username;
+	private String firstname;
+	private String lastname;
+	private Device[] device;
+	private String lastOnline;
+}
