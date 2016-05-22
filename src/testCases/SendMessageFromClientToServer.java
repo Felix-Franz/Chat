9bba05ffbb6499728b04517b5e98dddbd38b623f
Felix Franz <felix-franz@t-online.de>
2016-05-22 11:32:51 +0200

    Changed sleep time
    Changed sleep time for the thread from 1000 to 10 ms

--- src/testCases/SendMessageFromClientToServer.java
+++ src/testCases/SendMessageFromClientToServer.java
@@ -23,7 +23,7 @@ public class SendMessageFromClientToServer implements Runnable{
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
